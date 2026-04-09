99093c6914fc0720f63879f535acfcd6f153a201
rrchai <73901500+rrchai@users.noreply.github.com>
2026-04-09 16:15:44 +0000

    feat: bump DocumentDB from 5.0 to 8.0

--- src/docdb_stack.py
+++ src/docdb_stack.py
@@ -35,7 +35,7 @@ class DocdbStack(cdk.Stack):
         cluster_parameter_group = docdb.ClusterParameterGroup(
             self,
             "DocDbClusterParameterGroup",
-            family="docdb5.0",
+            family="docdb8.0",
             parameters={
                 "audit_logs": "disabled",
                 "profiler": "enabled",
